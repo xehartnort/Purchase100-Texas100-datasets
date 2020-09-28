482b706ed8d446dcc919b8c0067ae79201b7b92d
xehartnort <xehartnort@github.com>
2020-09-28 12:55:49 +0200

    add code, license and steps

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
-# Purchase100-dataset
-A repo to download and preprocess the Purchase100 dataset extracted from Kaggle: Acquire Valued Shoppers Challenge
+# Purchase100 dataset
+
+A repo to preprocess the Purchase100 dataset extracted from Kaggle: Acquire Valued Shoppers Challenge
+
+## Steps
+
+ 1. Download the `transactions.csv.gz` file from https://www.kaggle.com/c/acquire-valued-shoppers-challenge/data
+ 2. Run `preprocess_dataset.py <path_to_transactions.csv.gz>` to generate numpy binary files .npy
+ 3. In the root folder of your local copy of this repository, two new files are created: `purchase100-features.npy` and `purchase100-labels.npy`
+ 4. Now you can import them in your code using [numpy.load function](https://numpy.org/doc/stable/reference/generated/numpy.load.html)
+
+## References
+
+The code in this repo is based on the preprocessing scripts given in https://github.com/bargavj/EvaluatingDPML
---
+++ preprocess_dataset.py
@@ -0,0 +1,61 @@
+import operator
+import numpy as np
+from sklearn.preprocessing import normalize
+from sklearn.cluster import KMeans
+import gzip
+import sys
+
+IT_NUM = 100
+
+def normalizeDataset(X):
+    mods = np.linalg.norm(X, axis=1)
+    return X / mods[:, np.newaxis]
+
+def populate_and_make_dataset(dataset_path):    
+    cnt, cust_cnt, it_cnt = 0, 0, 0
+    items = dict()
+    customer = dict()
+    last_cust = ''
+    with gzip.open(dataset_path) as fp:
+        for line in fp:
+            cnt += 1
+            if cnt == 1:
+                continue
+            lin = str(line).split(',')
+            cust = lin[0]
+            it = lin[3]
+            if it not in items and it_cnt < IT_NUM:
+                items[it] = it_cnt
+                it_cnt += 1
+            if cust not in customer:
+                customer[cust] = [0]*IT_NUM
+                cust_cnt += 1
+                last_cust = cust
+            if cust_cnt > 250000:
+                break
+            if it in items:
+                customer[cust][items[it]] = 1
+    del customer[last_cust]
+
+    no_purchase = []
+    for key, val in customer.items():
+        if 1 not in val:
+            no_purchase.append(key)
+    for cus in no_purchase:
+        del customer[cus]
+
+    dataset = []
+    for key, val in customer.items():
+        dataset.append(val)
+    dataset = np.array(dataset)
+    dataset = normalizeDataset(dataset)
+
+    np.save('purchase_100_features.npy', dataset)
+    X = KMeans(n_clusters=100, random_state=0).fit(dataset)
+    np.save('purchase_100_labels.npy', X.labels_)
+    
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        exit("Error: A valid path to transaction.csv.gz file must be provided as argument")
+    else:
+        populate_and_make_dataset(sys.argv[1])
